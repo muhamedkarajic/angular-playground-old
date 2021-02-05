9a9a109469c829d2667ac5f98b0af339069d98de
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-02-05 23:01:15 +0100

    Initial clearning of unused parts of the app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from '../../components/app-component/app.component';
+import { AppComponent } from 'src/components/app-component/app.component';
 
 @NgModule({
   declarations: [AppComponent],
